7ca723d309238101acef6c601cf42618d581ff9e
Arkadiusz Juszczyk <arkadiuszjuszczyk@hotmail.com>
2024-03-12 21:19:45 +0100

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -10,8 +10,9 @@ export const config = {
 
 export function middleware(req: NextRequest) {
   let lng;
-  if (req.cookies.has(cookieName))
-    lng = acceptLanguage.get(req.cookies.get(cookieName).value);
+  if (req.cookies.has(cookieName)) {
+    lng = acceptLanguage.get(req.cookies.get(cookieName)?.value ?? '');
+  }
   if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'));
   if (!lng) lng = fallbackLng;
 
@@ -25,13 +26,16 @@ export function middleware(req: NextRequest) {
   }
 
   if (req.headers.has('referer')) {
-    const refererUrl = new URL(req.headers.get('referer'));
-    const lngInReferer = languages.find((l) =>
-      refererUrl.pathname.startsWith(`/${l}`)
-    );
-    const response = NextResponse.next();
-    if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
-    return response;
+    const refererHeaderValue = req.headers.get('referer');
+    if (refererHeaderValue) {
+      const refererUrl = new URL(refererHeaderValue);
+      const lngInReferer = languages.find((l) =>
+        refererUrl.pathname.startsWith(`/${l}`)
+      );
+      const response = NextResponse.next();
+      if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
+      return response;
+    }
   }
 
   return NextResponse.next();
